00276e6425398000aba35d1a754f63ba74769169
« Julien <j.noyer@digitalworkshop.fr>
2017-11-23 12:39:50 +0100

    Manipulation du DOM

--- 23nov/exo4/js/script.js
+++ 23nov/exo4/js/script.js
@@ -11,3 +11,32 @@ Création d'un objet
         }
     };
 //
+
+/*
+Sélectionner les balises HTML en Javascript
+*/
+    var chatBox = document.getElementById('chatBox');
+    var userMessage = document.getElementById('userMessage');
+    var messagesBox = document.getElementById('messagesBox');
+//
+
+/*
+Capter l'événement 'submit' du formulaire
+*/
+    chatBox.addEventListener( 'submit', function(evt){
+        // Bloquer le comportement par defaut du formulaire
+        evt.preventDefault();
+
+        // Récupérer la valeur du input et analyser le nombre de caractères
+        if( userMessage.value.length > 0 ){
+            console.log(userMessage.value);
+
+            // Modifier le contenu HTML de la ballise messageBox
+            messagesBox.innerHTML += '<article class="userMessage">'+ userMessage.value + '</article>'
+        };
+
+        // Vider la valeur de l'input
+        userMessage.value = '';
+        
+    });
+// 
