6beda04ef401cc383f98663a689180ce324a3e99
« Julien <j.noyer@digitalworkshop.fr>
2017-11-30 12:17:52 +0100

    Fonction ES6

--- es6/js/script.js
+++ es6/js/script.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 /*
 Les variables "classiques" s'écrivent "let"
 Le templating de string se fait avec des backquote
 */
-var username = "Thomas";
+var username = 'Thomas';
 //
 
 /*
@@ -18,8 +18,41 @@ var birthYear = 1979;
 Modifier le DOM
 */
 // Céer une variable pour sélectionner la balise #pageTitle
-var pageTitle = document.querySelector("#pageTitle");
+var pageTitle = document.querySelector('#pageTitle');
 
 // Modifier le contenu de la balise
-pageTitle.innerHTML = "\n        Bonjour <em>" + username + "</em>, vous avez " + (2017 - birthYear) + " ans\n    ";
+pageTitle.innerHTML = '\n        Bonjour <em>' + username + '</em>, vous avez ' + (2017 - birthYear) + ' ans\n    ';
+//
+
+
+/*
+Création d'une fonction
+*/
+// Fonction simple = un retour
+var sayHello = function sayHello() {
+    return console.log('Say Hello');
+};
+sayHello();
+
+// Fonction avec un paramètre = pas besoin de parenthèses
+var twice = function twice(uniqNumber) {
+    return console.log(uniqNumber * 2);
+};
+twice(10);
+twice(15);
+
+// Fonction avec plusieurs paramètres
+var simpleAddition = function simpleAddition(firstNumber, secondeNumber) {
+    var total = firstNumber + secondeNumber;
+    console.log(total);
+};
+simpleAddition(10, 45);
+
+// Fonction avec paramètre optionnel (valeur par defaut)
+var optionalParameter = function optionalParameter() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Julien';
+    return console.log(name);
+};
+optionalParameter();
+optionalParameter('Abdel');
 //
--- es6/typings/script.js
+++ es6/typings/script.js
@@ -23,3 +23,29 @@ Modifier le DOM
         Bonjour <em>${username}</em>, vous avez ${2017 - birthYear} ans
     `;
 //
+
+
+/*
+Création d'une fonction
+*/ 
+    // Fonction simple = un retour
+    const sayHello = () => console.log('Say Hello')
+    sayHello()
+
+    // Fonction avec un paramètre = pas besoin de parenthèses
+    const twice = uniqNumber => console.log( uniqNumber * 2 )
+    twice(10)
+    twice(15)
+
+    // Fonction avec plusieurs paramètres
+    const simpleAddition = (firstNumber, secondeNumber) => {
+        let total = firstNumber + secondeNumber
+        console.log(total)
+    }
+    simpleAddition(10, 45)
+
+    // Fonction avec paramètre optionnel (valeur par defaut)
+    const optionalParameter = (name = `Julien`) => console.log(name)
+    optionalParameter()
+    optionalParameter(`Abdel`)
+//
